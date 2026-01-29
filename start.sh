e01d782eb630c5667157b09eb49a5ecd22d8cb62
enamulhasan248 <enamulhasan248@gmail.com>
2026-01-29 19:33:03 +0600

    Fix start.sh: cd to core folder and enable Celery Beat scheduler

--- start.sh
+++ start.sh
@@ -4,7 +4,11 @@
 cd core
 
 # Start Celery in background
-celery -A core worker --loglevel=info --concurrency=2 &
+#celery -A core worker --loglevel=info --concurrency=2 &
+
+# Added -B to run the Beat scheduler embedded
+# Change
+celery -A core worker --beat --scheduler django --loglevel=info --concurrency=2 &
 
 # Start Gunicorn in foreground
 gunicorn core.wsgi:application
